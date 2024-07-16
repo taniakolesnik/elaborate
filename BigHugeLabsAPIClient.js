ff399ec070595146edb5b0c8f295d4be05088319
Tetiana Kolesnik <tetianakolesnik@Tetianas-MacBook-Pro.local>
2024-07-16 11:32:38 +0100

    get list of syns from BigHugeLabsAPIClient.js

--- BigHugeLabsAPIClient.js
+++ BigHugeLabsAPIClient.js
@@ -1,36 +1,27 @@
 import axios from 'axios';
 import { getAPIKey } from "./getAPIKey.js";
+import BigHugeLabsAPIDataParser from "./BigHugeLabsAPIDataParser"
 
 const BigHugeLabsAPIClient = async (inputMessage, secretWord) => {
-  const apiURL = 'https://api.openai.com/v1/chat/completions';
+  
   const apiKey = await getAPIKey("bighugelabsapi");
+  
+  const getList = async (word) => {
+    apiURL = `https://words.bighugelabs.com/api/2/${apiKey}/${word}`
+    
+    try {
+      const response = await axios.get(apiURL);
+      const list = BigHugeLabsAPIDataParser(response.data);
+      return list; 
 
-  // try {
-  //   const response = await axios.post(
+    } catch (error) {
+      console.error('Error calling BigHugeLabs API', error);
+      return 'Error: Unable to fetch response';
+    }
+  }
+s
+  return await getList(inputMessage)
 
-  //     apiURL,
-  //     {
-  //       model: "gpt-3.5-turbo",
-  //       messages: [
-  //         {role: "user", content: userContent}
-  //       ],
-  //       temperature: 0,  
-  //       top_p: 0.36,
-  //       max_tokens: 50
-  //     },
-  //     {
-  //       headers: {
-  //         'Authorization': `Bearer ${apiKey}`,
-  //         'Content-Type': 'application/json'
-  //       }
-  //     }
-  //   );
-    return apiKey; 
-
-  // } catch (error) {
-  //   console.error('Error calling OpenAI API:', error);
-  //   return 'Error: Unable to fetch response';
-  // }
 };
 
 export default BigHugeLabsAPIClient;
