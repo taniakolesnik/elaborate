98f45c6970e7347f4f51bdf3fea5625b15403037
Tetiana Kolesnik <tetianakolesnik@tetianas-mbp.home>
2024-07-25 21:51:49 +0100

    added stop words and return top desired number

--- getCommon.js
+++ getCommon.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const getCommon = async (inputMessage, secretWord) => {
   
-  const apiURL = "http://taniakolesnik.pythonanywhere.com/common_word?word1=" + inputMessage.toLowerCase() + "&word2=" + secretWord.toLowerCase();
+  const apiURL = "http://taniakolesnik.pythonanywhere.com/common_word?word1=" + inputMessage.toLowerCase() 
+  + "&word2=" + secretWord.toLowerCase() + "&top=5";
   console.log(apiURL)
   try {
     const response = await axios.get(apiURL);
