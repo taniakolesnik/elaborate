37d7e40af1a70afc8161296ca17fa609c8110e0f
Tetiana Kolesnik <tetianakolesnik@tetianas-mbp.home>
2024-07-28 13:35:59 +0100

    switched rules to modal

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Alert, Text, View } from 'react-native';
+import { StyleSheet, Alert, Text, View, Modal, Pressable } from 'react-native';
 import Game from './Game';
 import React, { useState } from 'react';
 import { MenuScreen } from './MenuScreen';
@@ -8,6 +8,16 @@ import { getData } from './asyncStorage';
 const App = () => {
 
   const [gameKey, setGameKey] = useState(0);
+  // https://reactnative.dev/docs/modal
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const objective = "Guess the secret English 5-letter word."  
+  const rules =  "You must submit a guess that is a 5-letter word also. \n\n" 
+  + "After each guess, you will receive feedback in the form of a list of the most common words that share letters with both your guess and the secret word.\n\n"
+  + "This feedback helps you narrow down the possible secret word.\n\n"
+  + "There is no limit to the number of guesses you can make.\n\n"
+  + "If you wish to give up, you can start a new game.\n\n"
+  + "The secret word of the current game will be revealed to you."
 
   const newGame = async (message) => {
     const secretWord = await getData("secretWord")
@@ -33,15 +43,43 @@ const App = () => {
     newGame(message)
   };
 
+  const showRules = () => {
+    setModalVisible(true)
+  };
+
   return (
 
     <MenuProvider>
       <View style={styles.container}>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalTextHeaders}>Objective: </Text>
+            <Text style={styles.modalText}>{objective}</Text>
+            <Text style={styles.modalTextHeaders}>Gameplay:</Text>
+            <Text style={styles.modalText}>{rules}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(false)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
         <View style={styles.topView}> 
           <Text style={styles.appTitle}>Elaborate</Text>
-          <MenuScreen onNewGameClick={giveUp}/>
+          <MenuScreen onNewGameClick={giveUp} onShowRulesClick={showRules}/>
         </View>
         <Game newGameStart={gameWin} key={gameKey}/>
+        
       </View>
     </MenuProvider>
   );
@@ -62,7 +100,48 @@ const styles = StyleSheet.create({
   topView: {
     flexDirection: 'row',
     justifyContent: 'space-between'
-  }
+  },  
+  modalView: {
+    marginVertical:'50%',
+    marginHorizontal:'10%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4
+    ,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+  },
+  modalTextHeaders: {
+    marginBottom: 15,
+    textAlign: 'left',
+    fontWeight: 'bold'
+  },
 });
 
 export default App;
--- MenuScreen.js
+++ MenuScreen.js
@@ -4,17 +4,16 @@ import {
     MenuOption,
     MenuTrigger, MenuProvider
   } from 'react-native-popup-menu';
-  import { StyleSheet, View, Alert, Text} from 'react-native';
+  import { StyleSheet, View, Alert, Text,} from 'react-native';
 
-  const howToPlayAlert = () => {
-    Alert.alert("how to play", "secret word is 5 letter word." 
-      + "Submit a guess 5 letter word to get the most common words between your guess and secret word. Until you find a secret word" 
-      + "Unlimited hints. "
-      + "You can play new game if you want to give up. Secret word will be revealed to you ")
-  }
+  // const howToPlayAlert = () => {
+  //   Alert.alert("how to play", "secret word is 5 letter word." 
+  //     + "Submit a guess 5 letter word to get the most common words between your guess and secret word. Until you find a secret word" 
+  //     + "Unlimited hints. "
+  //     + "You can play new game if you want to give up. Secret word will be revealed to you ")
+  // }
 
-  export const MenuScreen = ({onNewGameClick}) => (
-
+  export const MenuScreen = ({onNewGameClick, onShowRulesClick}) => (
 
     <View>
       <Menu>
@@ -24,9 +23,11 @@ import {
       </MenuTrigger>
         <MenuOptions>
           <MenuOption style={styles.menuItemStyle} onSelect={() => onNewGameClick()} text='Give up' />
-          <MenuOption style={styles.menuItemStyle} onSelect={() => howToPlayAlert()} text='Rules' />
+          <MenuOption style={styles.menuItemStyle} onSelect={() => onShowRulesClick()} text='Rules' />
         </MenuOptions>
       </Menu>
+
+      
     </View>
   );
 
