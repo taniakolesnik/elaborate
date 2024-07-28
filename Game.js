34b9f5c1b11d5dcdbe021f4136e8d0730870892e
Tetiana Kolesnik <tetianakolesnik@tetianas-mbp.home>
2024-07-28 14:26:31 +0100

    scroll to show last one

--- Game.js
+++ Game.js
@@ -4,7 +4,7 @@ import getRandomWord from './getRandomWord'
 import getCommon from './getCommon';
 import { getData, setData } from './asyncStorage';
 
-const Game = ({newGameStart}) => {
+const Game = ({ newGameStart }) => {
 
   const [attempts, setAttempts] = useState(0);
   const [guessList, setGuessList] = useState([]);
@@ -12,12 +12,13 @@ const Game = ({newGameStart}) => {
   const [isDisabledSendButton, setIsDisabledSendButton] = useState(true);
   const [isEnabledActivityIndicator, setIsEnabledActivityIndicator] = useState(false);
   const [secretWord, setSecretWord] = useState('');
+  const flatList = React.useRef(null)
 
   useEffect(() => {
-    setRandomWord();
+    getSecretWord();
   }, []);
 
-  const setRandomWord = async () => {
+  const getSecretWord = async () => {
     try {
       const response = await getRandomWord();
       if (response[0].length == 0) {
@@ -37,25 +38,25 @@ const Game = ({newGameStart}) => {
     return input.length === 5 && regex.test(input);
   };
 
-const handleInputChange = (input) => {
-      setInputGuess(input)
-      if (validateInput(input)) {
-        setIsDisabledSendButton(false);
-      } else {
-        setIsDisabledSendButton(true);
-      }
+  const handleInputChange = (input) => {
+    setInputGuess(input)
+    if (validateInput(input)) {
+      setIsDisabledSendButton(false);
+    } else {
+      setIsDisabledSendButton(true);
+    }
   };
 
   const checkGuessInput = async () => {
     setIsEnabledActivityIndicator(true);
     setIsDisabledSendButton(true)
-    if (secretWord == inputGuess){
+    if (secretWord == inputGuess) {
       newGameStart();
       setIsEnabledActivityIndicator(false)
     } else {
       const response = await getCommon(inputGuess, secretWord);
       setGuessList(guessList.concat(inputGuess + " : " + response))
-      setAttempts(attempts+1)
+      setAttempts(attempts + 1)
       setInputGuess("");
       setIsEnabledActivityIndicator(false);
       setIsEnabledActivityIndicator(false)
@@ -71,20 +72,25 @@ const handleInputChange = (input) => {
 
   return (
     <View style={styles.container}>
-      
+
 
       <View style={styles.attemptsCountStyle}>
         <Text style={styles.attemptsCountStyle}>Attempts: {attempts}</Text>
       </View>
 
-      <View style={styles.activityIndicatorStyle}>
-        <ActivityIndicator animating={isEnabledActivityIndicator} size="large" />
-      </View>
-        <FlatList
+      <FlatList
+        ref={flatList}
         data={guessList}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
+        // https://stackoverflow.com/questions/46304677/scrolltoend-after-update-data-for-flatlist
+        onContentSizeChange={() => {
+          flatList.current.scrollToEnd();
+      }}
       />
+     <View style={styles.activityIndicatorStyle}>
+        <ActivityIndicator animating={isEnabledActivityIndicator} size="large" />
+      </View>
       <View style={styles.guessInputHelperView}>
         <Text style={styles.guessInputHelperText}>Submit will be activated when the guess input is 5 characters long. </Text>
         <Text style={styles.guessInputHelperText}>No spaces or special characters are permitted.</Text>
@@ -108,7 +114,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     paddingTop: '5%',
-    paddingBottom:'10%',
+    paddingBottom: '10%',
     backgroundColor: '#fff',
     justifyContent: 'center'
   },
@@ -117,14 +123,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignContent: 'center',
     fontSize: 16,
-    marginBottom: 16,
+    marginBottom: 5,
     fontWeight: "300"
   },
   inputStyle: {
     height: "10%",
     borderColor: '#ccc',
     borderWidth: 1,
-    paddingHorizontal: 8
+    paddingHorizontal: 10
   },
   guessItemStyle: {
     padding: 20,
@@ -139,24 +145,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     padding: 10,
   },
-  secretWordViewStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
-    padding: 10,
-  },  
-  secretTextStyle: {
-    fontSize: 50
-  },
   guessInputHelperView: {
-    margin:10
+    margin: 10
   },
   guessInputHelperText: {
     fontSize: 13,
     color: 'grey'
   },
 
-  
+
 });
 
 export default Game;
