77923d343d76e1ee283e5b37bb2c435558998991
Tetiana Kolesnik <tetianakolesnik@tetianas-mbp.home>
2024-09-08 16:35:38 +0100

    deleted old files

--- old/react-native.config.js
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/questions/41825276/how-to-add-custom-font-in-react-native-android
-
-module.exports = {
-    project: {
-        ios: {},
-        android: {},
-    },
-    assets: ['./assets/fonts']
-    };
